803e838f0dc175957f2251dcc3b978a199626c5a
bougmaomar <bougmaomar2022@gmail.com>
2022-06-12 20:31:24 +0100

    nothing

--- src/app/admin/mail-form/mail-form.component.ts
+++ src/app/admin/mail-form/mail-form.component.ts
@@ -40,7 +40,6 @@ export class MailFormComponent implements OnInit {
   }
 
   Valider() {
-<<<<<<< HEAD
     if (this.mssgsMail.subject !== undefined && this.data.type) {
       this.adminService
         .AcceptMStage(this.data.id, this.mssgsMail)
@@ -79,26 +78,6 @@ export class MailFormComponent implements OnInit {
             );
           }
         });
-=======
-    if (this.mssgsMail.subject !== undefined) {
-      this.adminService.SendMail(this.mssgsMail).subscribe((ret: any) => {
-        if (ret == 1) {
-
-          this.dialogRef.close();
-          Swal.fire(
-            'Email envoyer',
-            'Email d acceptation de la demande Mission Stage a ete envoyer avec succes',
-            'success'
-          );
-        } else {
-          Swal.fire(
-            'Erreur',
-            'Email d acceptation ete pas envoyer veuillez verifier les champs',
-            'error'
-          );
-        }
-      });
->>>>>>> 10a4db0181e19489960f6ea2e931bc5a9f35a905
     } else {
       document.getElementById('sujet').textContent = 'Sujet peut pas etre vide';
     }
