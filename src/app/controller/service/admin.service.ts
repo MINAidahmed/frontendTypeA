884bfb82ac1009c4b9c9a8dec7e9289f76e0a11f
bougmaomar <bougmaomar2022@gmail.com>
2022-06-19 19:42:25 +0100

    nothin

--- src/app/controller/service/admin.service.ts
+++ src/app/controller/service/admin.service.ts
@@ -22,14 +22,8 @@ import { Montant_par_labo } from '../model/Montant_par_labo.model';
   providedIn: 'root',
 })
 export class AdminService {
-<<<<<<< HEAD
   private baseUrl = 'http://172.19.177.32:8080/admin';
   //private baseUrl = 'http://localhost:8080/admin';
-=======
-
-//  private baseUrl = 'http://172.19.177.32:8080/admin';
-  private baseUrl = 'http://localhost:8000/admin';
->>>>>>> f66019ae84313cbb6fb449b62894dfd76f88e863
 
   constructor(private httpClient: HttpClient) {}
 
@@ -390,9 +384,7 @@ export class AdminService {
   }
   find_montant_par_labo_par_year(year: number): Observable<Montant_par_labo[]> {
     return this.httpClient.get<Montant_par_labo[]>(
-
-      `${this.baseUrl + '/getmontant_par_labo_par_year/'+year}`
-
+      `${this.baseUrl + '/getmontant_par_labo_par_year/' + year}`
     );
   }
 
